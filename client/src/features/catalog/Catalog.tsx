339b726471d7d945d8422e97aa8520efc912f30d
Amit Adhikari  ameet <info.amitadhikari@gmail.com>
2024-12-26 13:18:37 +0000

    Error Handling

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -6,6 +6,7 @@ export default function Catalog() {
     const [products, setProducts] = useState<Product[]>([]);
 
     useEffect(() => {
+        //
       fetch('http://localhost:5000/api/products')
         .then(response => response.json())
         .then(data => setProducts(data));
